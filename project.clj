8756ce4e404e3aa802adbd6b91f5093501e5d983
Ronen Cohen <ronen.cohen@appsflyer.com>
2020-05-20 21:04:27 +0300

    Add clj-kondo and eftest

--- project.clj
+++ project.clj
@@ -14,6 +14,12 @@
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :profiles {:dev {:dependencies      [[org.clojure/clojure "1.10.1"]
                                        [com.google.protobuf/protobuf-java "3.11.3"]
-                                       [criterium "0.4.5"]]
-                   :java-source-paths ["src/java" "test/java"]}}
+                                       [criterium "0.4.5"]
+                                       [clj-kondo "RELEASE"]]
+                   :aliases           {"clj-kondo" ["run" "-m" "clj-kondo.main"]}
+                   :eftest            {:multithread?   false
+                                       :report         eftest.report.junit/report
+                                       :report-to-file "target/junit.xml"}
+                   :java-source-paths ["src/java" "test/java"]
+                   :plugins           [[lein-eftest "0.5.9"]]}}
   :repl-options {:init-ns pronto.core})
