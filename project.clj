b8f896e76f1f4bc847ccf2ff15e3641ba47ddebb
ran.solash <ran.solash@appsflyer.com>
2020-10-05 15:05:31 +0000

    create-map-with-vals

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pronto "1.0.5"
+(defproject pronto "1.0.6-SNAPSHOT"
   :description "clojure support for protocol buffers"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
