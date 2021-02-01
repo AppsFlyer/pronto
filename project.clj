246e3c9c37b9ac6413431e55c519078119ca121a
Ronen Cohen <ronen.cohen@appsflyer.com>
2021-02-01 18:58:45 +0200

    Version 1.0.17

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pronto "1.0.17-SNAPSHOT"
+(defproject pronto "1.0.17"
   :description "clojure support for protocol buffers"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
