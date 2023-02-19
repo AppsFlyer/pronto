e55f94072d2790e36e80e463c2300ad6927c1719
Idan <idan.tavor@appsflyer.com>
2023-02-19 10:34:21 +0200

    release version 2.1.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def protobuf-version "3.9.0")
 
 
-(defproject com.appsflyer/pronto "2.1.1-SNAPSHOT"
+(defproject com.appsflyer/pronto "2.1.1"
   :description "clojure support for protocol buffers"
   :url "https://github.com/AppsFlyer/pronto"
   :license {:name "Eclipse Public License"
